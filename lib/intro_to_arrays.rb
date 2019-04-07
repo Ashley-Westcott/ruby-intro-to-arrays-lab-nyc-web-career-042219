2be03903340b388197dc74f129b9bbf764167a39
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-07 03:46:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,3 +13,19 @@ end
 def third_element(array)
   return array[2]
 end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element(array)
+  return array.first
+end
+
+def last_element(array)
+  return array.last
+end
+
+def length_of_array(array)
+  return array.length
+end
